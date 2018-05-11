08082a184f7a0417bfe5febeeebcc4fdc7514727
Michał Piotrowski <97.piotrowski.michal@gmail.com>
2018-05-11 19:44:03 +0200

    Initial commit, configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+let path = require('path');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/js/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'assets/js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: '/./assets',
+              useRelativePath: true,
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('./assets/css/[name].css'),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3001,
+      files: ['./dist/*.html'],
+      server: { baseDir: ['dist'] }
+    }),
+  ]
+};
