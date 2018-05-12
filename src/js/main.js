5d9ebcde1d0a2d7dd96e21705d47327d03b5267d
Michał Piotrowski <97.piotrowski.michal@gmail.com>
2018-05-12 15:50:21 +0200

    Add styles for header

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 import '../sass/main.sass';
 import 'normalize-css';
+
+let header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (0 < window.scrollY) {
+    header.classList.add('scroll');
+  } else {
+    header.classList.remove('scroll');
+  }
+});
