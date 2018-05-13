cb5a0e31b11883a89f6ef5eaa3d1b549c6531952
Michał Piotrowski <97.piotrowski.michal@gmail.com>
2018-05-13 23:30:36 +0200

    Add 'about us' section, add smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import 'normalize-css';
 let header = document.querySelector('.header');
 let banner = document.querySelector('.banner');
 let hamburgerIcon = document.querySelector('.nav-btn');
+let navItems = document.querySelectorAll('.header-nav .nav-item');
 
 window.addEventListener('scroll', () => {
   if (0 < window.scrollY) {
@@ -16,4 +17,23 @@ window.addEventListener('scroll', () => {
 hamburgerIcon.addEventListener('click', (event) => {
   event.preventDefault();
   header.classList.toggle('nav-open');
+});
+
+navItems.forEach(item => {
+  item.addEventListener('click', (event) => {
+    if (header.classList.contains('nav-open')) {
+      header.classList.remove('nav-open');
+    }
+
+    let id = event.target.href.split("#")[1];
+    let offset = 139;
+    let target = document.querySelector(`#${id}`).offsetTop - offset;
+    window.scroll({
+      top: target,
+      left: 0,
+      behavior: 'smooth'
+    });
+
+    event.preventDefault();
+  });
 });
